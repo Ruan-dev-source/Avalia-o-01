208a8b7afe0c5d9a8eeaf90a55caa2d1578ada7a
Ruan-dev-source <ruan.coelho.2009@aluno.edu.pi.gov.br>
2025-12-01 10:13:23 -0300

    Add files via upload
    Atividades da avaliação um da Mentoria Tech!

---
+++ Atividade01.java
@@ -0,0 +1,11 @@
+//TIP Para <b>executar</b> o código, pressione <shortcut actionId="Run"/> ou
+// clique no ícone <icon src="AllIcons.Actions.Execute"/> no gutter.
+void main() {
+    var escola = "CETI Alfredo Carlos Alencar";
+    var nome = "Ruan";
+    var idade = "16";
+    var atividade = "atividade 01 da avaliação 01";
+
+    System.out.println("Estudo no " + escola + ", meu nome é " + nome + " e minha idade é " + idade + " anos. Essa é a " + atividade + ".");
+
+}
---
+++ Atividade02.java
@@ -0,0 +1,7 @@
+void main() {
+
+    var msg = "Essa é a segunda atividade da avaliação um da Mentoria Tech.";
+
+    System.out.println("Oi! " + msg);
+
+}
